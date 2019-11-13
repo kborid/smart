6ed06ae18cd4d30da886af2d22382d0dbe276be1
段伟 <duanwei@thunisoft.com>
2019-11-13 19:42:30 +0800

    hybrid flutter

--- settingHelper/src/main/java/com/thunisoft/app2/MainActivity.java
+++ settingHelper/src/main/java/com/thunisoft/app2/MainActivity.java
@@ -1,7 +1,10 @@
 package com.thunisoft.app2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
+import io.flutter.facade.Flutter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        View flutterView = Flutter.createView(
+//                this,
+//                getLifecycle(),
+//                "route1"
+//        );
+//        FrameLayout.LayoutParams layout = new FrameLayout.LayoutParams(600, 800);
+//        layout.leftMargin = 100;
+//        layout.topMargin = 400;
+//        addContentView(flutterView, layout);
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fl_flutter_view, Flutter.createFragment("route1"));
+        fragmentTransaction.commit();
     }
 }
